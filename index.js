008e2efb8abca979297dd57facfe5fca82faeef1
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-03-22 05:42:18 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const wordfromStart = word;
+  const wordfromEnd = word.split('').reverse().join('');
+  return wordfromStart === wordfromEnd;
 }
 
 /* 
   Add your pseudocode here
+  function isPalindrome with Parameter word:
+	 Set wordfromStart as word
+	 Set wordfromEnd as word, splitting it to chars, reversing them, and joining them to string
+	 if wordfromStart === wordfromEnd:
+		return true
+	otherwise:
+		return false
 */
 
 /*
   Add written explanation of your solution here
+  We simply set two variables: wordfromStart and wordfromEnd.
+  The first variable (wordfromStart) will be initialized to the word itself.
+  The second variable (wordfromEnd) 
+	will be initialized to the word but from end to start, split up to characters, reversing the characters
+	and then joining them to a string.
+  We compare both variables, and if their values are equal we return true.
+  If the values aren't equal we return false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +36,18 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+  
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("dad"));
+  
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hello"));
+  
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("php"));
 }
 
 module.exports = isPalindrome;
